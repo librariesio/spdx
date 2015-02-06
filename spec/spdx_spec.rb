c1854270e3d726adaf1c6946071dd7a065c59edb
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-06 00:58:43 +0000

    More specs

--- spec/spdx_spec.rb
+++ spec/spdx_spec.rb
@@ -16,6 +16,10 @@ describe Spdx do
       expect(Spdx.find('Apache2').name).to eq("Apache License 2.0")
       expect(Spdx.find('Apache License, Version 2.0').name).to eq("Apache License 2.0")
       expect(Spdx.find('Educational Community License, Version 2.0').name).to eq("Educational Community License v2.0")
+      expect(Spdx.find('CDDL + GPLv2 with classpath exception').name).to eq("GNU General Public License v2.0 w/Classpath exception")
+      expect(Spdx.find('The MIT License').name).to eq("MIT License")
+      # expect(Spdx.find('BSD3').name).to eq('BSD 3-clause "New" or "Revised" License')
+      # expect(Spdx.find('GNU LESSER GENERAL PUBLIC LICENSE').name).to eq("GNU General Public License v1.0 only")
     end
   end
 end

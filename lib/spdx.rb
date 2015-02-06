5ab4025e45ef0ffcd1350acefe3ec53d6435959a
Andrew Nesbitt <andrewnez@gmail.com>
2015-02-06 00:38:07 +0000

    Implementation

--- lib/spdx.rb
+++ lib/spdx.rb
@@ -1,5 +1,25 @@
 require "spdx/version"
+require "spdx-licenses"
+require "text"
 
 module Spdx
-  # Your code goes here...
+  def self.find(name)
+    SpdxLicenses[name] || closest(name)
+  end
+
+  def self.closest(name)
+    match = names.sort_by do |key|
+      Text::Levenshtein.distance(name, key)
+    end.first
+    SpdxLicenses[match] || find_by_name(match)
+  end
+
+  def self.find_by_name(name)
+    match = SpdxLicenses.data.find{|k,v| v['name'] == name }
+    SpdxLicenses[match[0]] if match
+  end
+
+  def self.names
+    SpdxLicenses.data.keys + SpdxLicenses.data.map{|k,v| v['name'] }
+  end
 end

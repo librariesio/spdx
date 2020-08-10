1ff7a798de45e035d611e0d7742661d246a9e6b4
Matt Ellis <mellis@tidelift.com>
2020-08-10 14:59:46 -0400

    update for comments

--- lib/spdx.rb
+++ lib/spdx.rb
@@ -17,6 +17,7 @@ module Spdx
     unless defined?(@exceptions)
       data = JSON.parse(File.read(File.expand_path("../exceptions.json", __dir__)))
       @exceptions = {}
+
       data["exceptions"].each do |details|
         id = details.delete("licenseExceptionId")
         @exceptions[id] = details
@@ -26,23 +27,40 @@ module Spdx
   end
 
   def self.license_exists?(id)
-    licenses.key?(id.to_s) || licenses.keys.one? { |key| key.downcase == id.downcase }
+    licenses.key?(id.to_s) || licenses_downcase.key?(id.to_s.downcase)
   end
 
   def self.exception_exists?(id)
-    exceptions.key?(id.to_s) || exceptions.keys.one? { |key| key.downcase == id.downcase }
+    exceptions.key?(id.to_s) || exceptions_downcase.key?(id.to_s.downcase)
   end
 
   def self.licenses
     unless defined?(@licenses)
       data = JSON.parse(File.read(File.expand_path("../licenses.json", __dir__)))
       @licenses = {}
+
       data["licenses"].each do |details|
         id = details.delete("licenseId")
         @licenses[id] = details
       end
     end
     @licenses
+  end
+
+  def self.licenses_downcase
+    unless defined?(@licenses_downcase)
+      @licenses_downcase = {}
+      licenses.keys.each { |key| @licenses_downcase[key.downcase] = key }
+    end
+    @licenses_downcase
+  end
+
+  def self.exceptions_downcase
+    unless defined?(@exceptions_downcase)
+      @exceptions_downcase = {}
+      exceptions.keys.each { |key| @exceptions_downcase[key.downcase] = key }
+    end
+    @exceptions_downcase
   end
 
   def self.valid_spdx?(spdx_string)
